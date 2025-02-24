8838ca78f1e4aff8d4762a0ee5f19a3e436e650d
Cesar Acosta <c3sar.adan@gmail.com>
2025-02-24 09:53:53 -0600

    24/02/2025 pt 2

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas_20251/models/popular_response.dart';
+import '../models/credits_response.dart';
 import '../models/movie.dart';
+import '../models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _baseUrl = 'api.themoviedb.org';
@@ -9,4 +14,36 @@ class MoviesProvider extends ChangeNotifier {
 
   List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
+  Map<int, List<Cast>> moviesCast = {};
+
+  MoviesProvider() {
+    getOnDisplayMovies();
+    getPopularMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+    //print(decodeData);
+    //print(response.body);
+    final nowPLayingResponse = NowPlayingResponse.fromRawJson(response.body);
+    onDisplayMovies = nowPLayingResponse.results;
+    //Le comunicamos a todos los widgets que estan escuchando que se cambio la data por lo tanto se tienen que redibujar
+    notifyListeners();
+    //print(nowPLayingResponse.results[0].title);
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+    final response = await http.get(url);
+
+    final popularResponse = PopularResponse.fromRawJson(response.body);
+
+    popularMovies = [...popularMovies, ...popularResponse.results];
+    notifyListeners();
+  }
 }
