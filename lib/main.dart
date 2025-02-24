25c05250dc8ef265ca914408e6172c04e4aaad72
Cesar Acosta <c3sar.adan@gmail.com>
2025-02-24 09:18:55 -0600

    24/02/2025 pt 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'providers/movies_provider.dart';
 import 'screens/screens.dart';
 
 void main() => runApp(const AppState());
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/movie.dart';
+
 class MoviesProvider extends ChangeNotifier {
   String _baseUrl = 'api.themoviedb.org';
   String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
